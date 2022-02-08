5e8cc08dc86daee9cb380316e6bc0ee7b738c4df
Berk Bozkurt <BBozkurt@lcs.on.ca>
2022-02-08 10:03:35 -0500

    Added a function to check the answer to see if it is correct or not.
    cannot run the simulator because of software update

--- Arithmetic Ace/ClassExample.swift
+++ Arithmetic Ace/ClassExample.swift
@@ -9,9 +9,14 @@ import SwiftUI
 
 struct ClassExample: View {
     // MARK: Stored Properties
-    let multiplicand = Int.random(in: 1...12)
-    let multiplier = Int.random(in: 1...12)
+   @State var multiplicand = Int.random(in: 1...12)
+   @State var multiplier = Int.random(in: 1...12)
     @State var inputGiven = ""
+    
+    // Has an answer been checked?
+    @State var answerChecked = false
+    // Was the answer given actually correct?
+    @State var answerCorrect = false
     
     
     // MARK: Computed Property
@@ -41,22 +46,35 @@ struct ClassExample: View {
                 
                 Image(systemName: "checkmark.circle")
                     .foregroundColor(.green)
+                //              Condition   true  false
+                    .opacity(answerCorrect ? 1.0 : 0.0)
                 Spacer()
                 TextField("",
                           text: $inputGiven)
                     .multilineTextAlignment(.trailing)
             }
+          
+            
             Button(action: {
+                
+                // Answer has been checked!
+                answerChecked = true
+                
+                
+                
                 // Convert the input given to an integer, if possible
                 guard let productGiven = Int(inputGiven) else {
                     // Sadness, not a number
+                    answerCorrect = false
                     return
                 }
                 // Check the answer!
                 if productGiven == correctProduct {
                     // Celebrate!
+                    answerCorrect = true
                 } else {
-                    // Sadness, they gave a number, but it's not correct. 
+                    // Sadness, they gave a number, but it's not correct.
+                    answerCorrect = false
                 }
                 
             }, label: {
@@ -67,15 +85,32 @@ struct ClassExample: View {
                 .padding()
                 .buttonStyle(.bordered)
             
-            
-            
-            Spacer()
-        }
-        .padding(.horizontal)
-        .font(.system(size: 72))
+            Button(action: { Multiplicand()
+                    }, label: {
+            Text("New question")
+                .font(.largeTitle)
+            })
+                .padding()
+                .buttonStyle(.bordered)
+        //        CONDITION      true  false
+        .opacity(answerCorrect ? 1.0 : 0.0)
+                Spacer()
+                    }
+                .padding(.horizontal)
+                .font(.system(size: 72))
+
+                }
+    func Multiplicand() {
+        multiplicand = Int.random(in: 1...12)
+        multiplier = Int.random(in: 1...12)
+        answerChecked = false
+        answerCorrect = false
+        inputGiven = ""
         
     }
-}
+            
+        }
+    
 
 struct ClassExample_Previews: PreviewProvider {
     static var previews: some View {
